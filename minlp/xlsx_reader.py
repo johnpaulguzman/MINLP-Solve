0277efcd8e649e47749fa4e0b9e33a1ead82a623
falcomaster <japseow@gmail.com>
2017-06-13 22:32:41 +0800

    refactored xlsx reader

--- minlp/xlsx_reader.py
+++ minlp/xlsx_reader.py
@@ -1,14 +1,22 @@
 import openpyxl
+import os
+#import code; code.interact(local=locals())
 
 # to interactively run this file, execute the following on IDLE
 # exec(open(r'C:\Users\pu\Pictures\MINLP-Solve\minlp\xlsx_reader.py').read(), globals())
 
-xlsx_path = r'C:\Users\pu\Pictures\MINLP-Solve\minlp\Parameters.xlsx'
+current_dir = os.path.split(os.path.abspath(__file__))[0]
+xlsx_path = current_dir + r"\Parameters.xlsx"
 book = openpyxl.load_workbook(xlsx_path, data_only=True)
+info_delimiter = "&"
+P = {} # Parameters
 
 prev_row_head = None
 for w in book.worksheets[0:1]:
     for row in w.values:
-        if prev_row_head is None: print("Prev is None")
-        print("Current is ", row)
+        if prev_row_head is None:
+            print("Prev is None")
+            P_key, *key_info = row[0].split(info_delimiter)
         prev_row_head = row[0]
+        if row[0] is None: continue
+        print("Current P_key, misc_info: ", P_key, key_info)
